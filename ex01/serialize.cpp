6ef72b7e162a01beadece5cbf67d6758dfbb76ef
Abde Nnour Chat <abdnourchatte99@gmail.com>
2026-01-21 22:04:21 +0100

    lol

---
+++ ex01/serialize.cpp
@@ -0,0 +1,12 @@
+#pragma once 
+
+class serialize
+{
+public:
+    serialize();
+    serialize();
+    serialize(const serialize &) = default;
+    serialize &operator=(serialize &&) = default;
+    serialize &operator=(const serialize &) = default;
+    ~serialize();    
+};
